6fc13ede24ee6342268df604535f3ee0d8373fd3
Tejasvi Kathuria <tkat3009@gmail.com>
2026-04-09 18:58:48 -0700

    match df size

--- analysis.R
+++ analysis.R
@@ -10,6 +10,9 @@ us  <- read_csv("data_processed/us_model_common_window.csv")
 # Keep only complete rows
 can <- can[complete.cases(can), ]
 us  <- us[complete.cases(us), ]
+can <- head(can, -4)
+us <- head(us, -1)
+
 
 # -------------------------
 # 2. Train / holdout split
